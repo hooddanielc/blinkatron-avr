0e6b1fd7b782ac85e7babc2cb7e0c852d360dde0
Daniel Hood <hood.danielc@gmail.com>
2016-12-11 15:38:35 -0800

    Add lick test framework

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-ib main
+TEST_TARGETS=test/*-test.cc
+
+for f in $TEST_TARGETS
+do
+  ib --cfg test "${f%.*}"
+  ../out/test/`realpath --relative-to . "${f%.*}"`
+done
+
+ib  main
--- main.cc
+++ main.cc
@@ -3,12 +3,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {  
+const int speed = 100;
+const int x = 0b11111100;
+
+int main(void) {
   DDRC |= (1 << PC5);
+  DDRC |= (1 << PC4);
+  DDRC |= (1 << PC3);
+  DDRC |= (1 << PC2);
 
   while (1) {
     PORTC ^= (1 << PC5);
-    _delay_ms(1000);
+    _delay_ms(speed);
+    PORTC ^= (1 << PC4);
+    _delay_ms(speed);
+    PORTC ^= (1 << PC3);
+    _delay_ms(speed);
+    PORTC ^= (1 << PC3);
+    _delay_ms(speed);
   }
 
   return 0;
